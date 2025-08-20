c6b68dbeed9ca2147640b2a7189124a444c0cc76
Rasikpur <rasikapur369gmail.com>
2025-08-20 12:51:36 +0530

    first commit

---
+++ rasi-workflow/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+     return "Flask App Deployed with CI/CD pipeline!"
+     return "New version deployed!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
